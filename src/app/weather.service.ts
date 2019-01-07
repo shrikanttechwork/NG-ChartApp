046578568499386ad92fa64a0c6fc7f0a89ba513
shrikanttechwork <shrimahajan0@gmail.com>
2019-01-07 23:19:45 +0530

    first commit

---
+++ src/app/weather.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private _http:HttpClient) { }
+
+  dailyForecast() {
+        const headers = new Headers();
+        headers.append('Access-Control-Allow-Headers', 'Content-Type');
+        headers.append('Access-Control-Allow-Methods', 'GET');
+        headers.append('Access-Control-Allow-Origin', '*');
+
+    return this._http.get("http://samples.openweathermap.org/data/2.5/history/city?q=Warren,OH&appid=b6907d289e10d714a6e88b30761fae22")
+    .pipe(map(result => result));
+  }
+
+}
